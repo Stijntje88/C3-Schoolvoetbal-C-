8f38886aeebe275d06504152e0a4da2a30292e3a
Stijntje88 <Stijnvandaelen@outlook.com>
2024-11-13 09:58:25 +0100

    c# c3

---
+++ c# schoolvoetbal c3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c__schoolvoetbal_c3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
